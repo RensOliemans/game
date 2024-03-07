4aa39af7f5e1469d0de0cd9d0e6cbacd18b5b3f2
Rens Oliemans <hallo@rensoliemans.nl>
2024-03-07 13:23:56 +0100

    Update About modal

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,12 +9,18 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source word guessing game -{' '}
+        This version of Mastermind is played with letters and words, just like
+        a version of the original <a
+          href="https://en.wikipedia.org/wiki/Bulls_and_cows#The_word_version"
+          className="underline font-bold"
+        >Bulls and cows</a> game, the original inspiration of Mastermind.
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
         <a
-          href="https://github.com/RensOliemans/Mastermind-letters"
+          href="https://github.com/RensOliemans/mastermind-letters"
           className="underline font-bold"
         >
-          check out the code here
+          Source code
         </a>{' '}
       </p>
     </BaseModal>
